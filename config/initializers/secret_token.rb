770d661709526b1292e249a67412f6e5c62c3f99
omnimal <malamasgr@yahoo.gr>
2013-11-15 11:04:48 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AAAWebsitemsc32::Application.config.secret_token = '333bb6f4a84a2429229c4118dee7aa5c06a0603b265e2238025b41bc733c6c3e017d60c9043f142f0befb0777463d03a056207959b42991e1f118851d4c699b9'
